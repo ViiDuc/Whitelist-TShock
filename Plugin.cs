62425b5503f5784ec406c2ad95f25f4aefc040e6
Frm.22 <aduc77716@gmail.com>
2024-11-07 01:29:01 +0000

    Update all

--- Plugin.cs
+++ Plugin.cs
@@ -1,103 +1,125 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 using Terraria;
 using TerrariaApi.Server;
-using System.Linq;
-using System.Collections.Generic;
 using TShockAPI;
-using Newtonsoft.Json;
-using Microsoft.Xna.Framework;
-using System.Drawing;
 
-[ApiVersion(2, 1)]
-namespace TShockPluginWhitelist
+namespace WhitelistPlugin
 {
+    [ApiVersion(2, 1)]
     public class WhitelistPlugin : TerrariaPlugin
     {
-        public override string Name => "WhitelistPlugin";
-        public override string Author => "ViiDuc";
-        public override string Description => "A plugin to manage a whitelist for the server.";
-        public override Version Version => new Version(1, 0, 0, 0);
+        private readonly string WhitelistFilePath = Path.Combine("tshock", "whitelist.json");
+        private HashSet<string> whitelistedPlayers;
 
-        private const string WhitelistFile = "whitelist.json";
+        public override string Name => "Whitelist Plugin";
+        public override string Author => "Your Name";
+        public override string Description => "A simple whitelist plugin for TShock 5.2";
+        public override Version Version => new Version(1, 0);
 
-        private List<string> Whitelist { get; set; } = new List<string>();
-
-        public WhitelistPlugin(Main game) : base(game)
+        public WhitelistPlugin(Main game) : base(game) 
         {
+            whitelistedPlayers = new HashSet<string>();
         }
 
         public override void Initialize()
         {
-            TShockAPI.Hooks.PlayerHooks.PlayerPostLogin += OnPlayerLogin;
-            Commands.ChatCommands.Add(new Command("whitelist.reload", ReloadWhitelist, "wl", "reload"));
-            Commands.ChatCommands.Add(new Command("whitelist.add", AddToWhitelist, "wt", "add"));
+            ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreetPlayer);
+            Commands.ChatCommands.Add(new Command("whitelist.reload", ReloadWhitelist, "wlreload"));
+            Commands.ChatCommands.Add(new Command("whitelist.add", AddToWhitelist, "wladd"));
+
+            try
+            {
+                LoadWhitelist();
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.ConsoleError($"[WhitelistPlugin] Error loading whitelist: {ex.Message}");
+            }
         }
 
-        private void OnPlayerLogin(TShockAPI.Hooks.PlayerPostLoginEventArgs e)
+        protected override void Dispose(bool disposing)
         {
-            if (!Whitelist.Contains(e.Player.Name.ToLower()))
+            if (disposing)
             {
-                e.Player.SendMessage("You are not whitelisted on this server.", Microsoft.Xna.Framework.Color.Red);
-                e.Player.Kick("You are not whitelisted.");
+                ServerApi.Hooks.NetGreetPlayer.Deregister(this, OnGreetPlayer);
             }
+            base.Dispose(disposing);
+        }
+
+        private void OnGreetPlayer(GreetPlayerEventArgs args)
+        {
+            var player = TShock.Players[args.Who];
+            if (player == null)
+            {
+                return;
+            }
+
+            if (!whitelistedPlayers.Contains(player.Name.ToLower()))
+            {
+                player.Disconnect("You are not whitelisted on this server.");
+                return;
+            }
+        }
+
+        private void LoadWhitelist()
+        {
+            if (!File.Exists(WhitelistFilePath))
+            {
+                Directory.CreateDirectory("tshock"); // Tạo thư mục tshock nếu chưa tồn tại
+                File.WriteAllText(WhitelistFilePath, JsonConvert.SerializeObject(new List<string>(), Formatting.Indented));
+            }
+
+            var whitelistData = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText(WhitelistFilePath));
+            whitelistedPlayers = new HashSet<string>(whitelistData.ConvertAll(p => p.ToLower()));
         }
 
         private void ReloadWhitelist(CommandArgs args)
         {
             try
             {
-                var path = Path.Combine(TShockAPI.TShock.SavePath, WhitelistFile);
-                if (File.Exists(path))
-                {
-                    string json = File.ReadAllText(path);
-                    Whitelist = JsonConvert.DeserializeObject<List<string>>(json);
-                    args.Player.SendMessage("Whitelist reloaded successfully.", Microsoft.Xna.Framework.Color.Green);
-                }
-                else
-                {
-                    args.Player.SendMessage("Whitelist file not found, creating new file.", Microsoft.Xna.Framework.Color.Yellow);
-                    SaveWhitelist();
-                }
+                LoadWhitelist();
+                args.Player.SendSuccessMessage("Whitelist reloaded.");
             }
             catch (Exception ex)
             {
-                args.Player.SendMessage($"Error reloading whitelist: {ex.Message}", Microsoft.Xna.Framework.Color.Red);
+                args.Player.SendErrorMessage($"Error reloading whitelist: {ex.Message}");
             }
         }
 
         private void AddToWhitelist(CommandArgs args)
         {
-            if (args.Parameters.Count == 0)
+            if (args.Parameters.Count < 1)
             {
-                args.Player.SendMessage("Usage: /wt add <username>", Microsoft.Xna.Framework.Color.Red);
+                args.Player.SendErrorMessage("Usage: /wladd <playername>");
                 return;
             }
 
-            string username = args.Parameters[0].ToLower();
-            if (Whitelist.Contains(username))
+            var playerName = args.Parameters[0].ToLower();
+            if (!whitelistedPlayers.Contains(playerName))
             {
-                args.Player.SendMessage($"{username} is already in the whitelist.", Microsoft.Xna.Framework.Color.Yellow);
-                return;
+                whitelistedPlayers.Add(playerName);
+                try
+                {
+                    SaveWhitelist();
+                    args.Player.SendSuccessMessage($"{args.Parameters[0]} has been added to the whitelist.");
+                }
+                catch (Exception ex)
+                {
+                    args.Player.SendErrorMessage($"Error saving whitelist: {ex.Message}");
+                }
             }
-            Whitelist.Add(username);
-            SaveWhitelist();
-
-            args.Player.SendMessage($"{username} has been added to the whitelist.", Microsoft.Xna.Framework.Color.Green);
+            else
+            {
+                args.Player.SendErrorMessage($"{args.Parameters[0]} is already whitelisted.");
+            }
         }
 
         private void SaveWhitelist()
         {
-            try
-            {
-                var path = Path.Combine(TShockAPI.TShock.SavePath, WhitelistFile);
-                string json = JsonConvert.SerializeObject(Whitelist, Formatting.Indented);
-                File.WriteAllText(path, json);
-            }
-            catch (Exception ex)
-            {
-                TShockAPI.TShock.Log.Error($"Error saving whitelist: {ex.Message}");
-            }
+            File.WriteAllText(WhitelistFilePath, JsonConvert.SerializeObject(whitelistedPlayers, Formatting.Indented));
         }
     }
 }
